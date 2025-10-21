a6cba69e0c79edc74f8974564503a982e46764c5
Danrhy <danrhy@gmail.com>
2025-10-21 13:37:49 -0300

    Atualizado ok!

--- server/api.js
+++ server/api.js
@@ -2,7 +2,7 @@ import react from "react";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://10.122.41.144:3000/',
+  baseURL: 'https://68e447c28e116898997b7339.mockapi.io/teste/',
   
 });
 
